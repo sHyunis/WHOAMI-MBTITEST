2f7d2c4fb622d3c76163df4a0fadea4e2d699cd7
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-09-12 00:40:24 +0900

    server수정

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// server.js
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+
+const PORT = process.env.PORT || 3000; // PORT 환경 변수 사용
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
