1b22405957d8cf95ccc94b6fe4464f7577fa2987
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-09-12 00:43:46 +0900

    수정

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // db.json 파일은 프로젝트 루트에 위치해야 합니다
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000; // PORT 환경 변수 사용
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+module.exports = (req, res) => {
+  server(req, res);
+};
