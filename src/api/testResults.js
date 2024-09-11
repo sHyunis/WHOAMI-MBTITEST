78b9d3b73dfa40021e7af008e2c492c42b5a9fc2
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-09-12 00:30:16 +0900

    수정

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -8,8 +8,8 @@ export const getTestResults = async () => {
     const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
-    console.error("Failed to fetch test results:", error);
-    throw new Error("Failed to fetch test results.");
+    console.error("결과 실패", error);
+    throw new Error("결과 실패");
   }
 };
 
@@ -19,8 +19,8 @@ export const createTestResult = async (resultData) => {
     const response = await axios.post(API_URL, resultData);
     return response.data;
   } catch (error) {
-    console.error("Failed to create test result:", error);
-    throw new Error("Failed to create test result.");
+    console.error("결과 실패", error);
+    throw new Error("결과 실패");
   }
 };
 
@@ -30,18 +30,18 @@ export const deleteTestResult = async (id) => {
     const response = await axios.delete(`${API_URL}/${id}`);
     return response.data;
   } catch (error) {
-    console.error("Failed to delete test result:", error);
-    throw new Error("Failed to delete test result.");
+    console.error("결과 실패", error);
+    throw new Error("결과 실패");
   }
 };
 
 // 테스트 결과 공개 업데이트
 export const updateTestResultVisibility = async (id, visibility) => {
   try {
-    const response = await axios.patch(`${API_URL}/${id}`, { visibility }); // 수정된 부분
+    const response = await axios.patch(`${API_URL}/${id}`, { visibility });
     return response.data;
   } catch (error) {
-    console.error("Failed to update test result visibility:", error);
-    throw new Error("Failed to update test result visibility.");
+    console.error("결과 실패", error);
+    throw new Error("결과 실패");
   }
 };
