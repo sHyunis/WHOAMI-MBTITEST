8353096bf96d582a1d12555710ffe6dc92e5d7d5
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-09-12 00:46:15 +0900

    경로수정

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://whoami-mbtitest.vercel.app/testResults";
+const API_URL = "http://localhost:4000/testResults";
 
 // 테스트 결과 가져오기
 export const getTestResults = async () => {
